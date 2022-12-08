936ff70eef33bfcdd8ad6aa19e0ed7aa20505937
id2k1149 <id2k1149@gmail.com>
2022-12-08 13:34:22 -0500

    Initial Commit

---
+++ TwoViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViews
+//
+//  Created by Max Franz Immelmann on 12/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
